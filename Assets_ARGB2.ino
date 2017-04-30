93000582a05d706cd15c3151ffe468effa66f625
Playyer96 <danilo.vanegas@upb.edu.co>
2017-04-30 17:56:41 -0500

    dep

--- Assets_ARGB2.ino
+++ Assets_ARGB2.ino
@@ -2,43 +2,108 @@
 #include <SPI.h>
 #include <GD2.h>
 
-#include "walk_assets.h"
+#include "assets.h"
 #include "Bat.h"
 #include "Samsong.h"
 
 Samsong samsong;
 
 int x;
+int state = 0;
+int count = 0;
 
-void setup() {
+void setup()
+{
   GD.begin();
   LOAD_ASSETS();
-
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+
+  static uint8_t i = 0;
+  static uint8_t j = 0;
+  static int16_t z = 488;
+  static uint16_t k = 0;
+  static int16_t xspeed = 2;
+  static int16_t xdir = -1;
 
   GD.Clear();
   GD.Begin(LINES);
-  GD.ColorRGB(255, 255, 0);
+  //GD.ColorRGB(255, 255, 0);
   GD.LineWidth(12 * 8);
   GD.Begin(LINE_STRIP);
   drawWave();
+  GD.ColorRGB(255, 255, 255);
+
+  x++;
+
+  GD.Begin(BITMAPS);
+  GD.Vertex2ii(0, 0,BackgroundS_HANDLE);
+  GD.Vertex2ii(z, k + 136, mAmarillo_HANDLE, j);
+
+  // if (state == 1)
+  // {
+  //   GD.Vertex2ii(30, 136, Golpe_HANDLE, j);
+  // }
+
+  if (state == 0)
+  {
+    GD.Vertex2ii(30, 136, Idle_HANDLE, j);
+  }
+
+  // if (state == 2)
+  // {
+  //   GD.Vertex2ii(30, 180, Muerte_HANDLE, j);
+  // }
   GD.swap();
 
-  x ++;
-  
+  // Animation speed
+  i++;
+  if (i == 20)
+  {
+    i = 0;
+    j = (j + 1) % 2;
+    count += 1;
+  }
+  if (count == 1)
+  {
+    state = 0;
+  }
+  // if (count == 5)
+  // {
+  //   state = 0;
+  //   count = 0;
+  // }
+
+  // movex and movey speed screen's size is 480x272
+  z = z + xspeed * xdir;
+  if (z < 0)
+  {
+    xdir = 1;
+  }
+  if (z <= 0)
+  {
+    z = 488;
+  }
 }
 
-void drawAttack() {
-  if (samsong.attack == 'B') {
+void drawAttack()
+{
+  if (samsong.attack == 'B')
+  {
 
   }
 }
 
-void drawWave() {
-  GD.Vertex2ii(140 + x - 10, 40); GD.Vertex2ii(140 + x + 20, 60);
-  GD.Vertex2ii(140 + x + 40, 100); GD.Vertex2ii(140 + x + 40, 140);
-  GD.Vertex2ii(140 + x + 25, 180); GD.Vertex2ii(140 + x + 10, 200);
+void drawWave()
+{
+  GD.Vertex2ii(140 + x - 10, 40);
+  GD.Vertex2ii(140 + x + 20, 60);
+  GD.Vertex2ii(140 + x + 40, 100);
+  GD.Vertex2ii(140 + x + 40, 140);
+  GD.Vertex2ii(140 + x + 25, 180);
+  GD.Vertex2ii(140 + x + 10, 200);
   GD.Vertex2ii(140 + x - 10, 220);
 }
--- assets.h
+++ assets.h
@@ -1,4 +1,4 @@
-#define LOAD_ASSETS()  GD.safeload("myassets.gd2");
+#define LOAD_ASSETS() GD.safeload("myassets.gd2");
 #define Idle_HANDLE 0
 #define Idle_WIDTH 160
 #define Idle_HEIGHT 140
@@ -15,8 +15,13 @@
 #define mAmarillo_WIDTH 60
 #define mAmarillo_HEIGHT 50
 #define mAmarillo_CELLS 2
-#define ASSETS_END 232900UL
+#define BackgroundS_HANDLE 4
+#define BackgroundS_WIDTH 479
+#define BackgroundS_HEIGHT 269
+#define BackgroundS_CELLS 1
+#define ASSETS_END 245301UL
 static const shape_t Idle_SHAPE = {0, 160, 140, 0};
 static const shape_t Golpe_SHAPE = {1, 185, 130, 0};
 static const shape_t Muerte_SHAPE = {2, 195, 90, 0};
 static const shape_t mAmarillo_SHAPE = {3, 60, 50, 0};
+static const shape_t BackgroundS_SHAPE = {4, 479, 269, 0};
